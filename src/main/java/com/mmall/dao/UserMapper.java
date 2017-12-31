9d642dfc1be94c38b35252186f50afa541a2775c
Jacky <huangjiaqi2397056Hjq@gmail.com>
2017-12-30 19:30:59 -0800

    let it go

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -44,4 +44,6 @@ public interface UserMapper {
     int updatePasswordByUsername(@Param("username")String username, @Param("passwordNew")String passwordNew);
 
     int checkPassword(@Param(value="password")String password, @Param("userId")Integer userId);
+
+    int checkEmailByUserId(@Param(value="email")String email, @Param(value="userId")Integer userId);
 }
